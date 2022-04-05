f94480d593b284f5f521abf186c8b5eaa124139a
jonorga <55115647+jonorgan@users.noreply.github.com>
2022-04-05 15:07:55 -0400

    added more sections

--- fir.py
+++ fir.py
@@ -1,3 +1,7 @@
+### TODO
+# Go here for instructions: https://www.w3.org/TR/PNG/
+# Look at sect 5.6 "Chunk Ordering" for links to all subs
+
 import struct
 
 f = open("test.png", 'rb')
@@ -5,14 +9,14 @@ raw_data = f.read()
 
 print("\n\nRaw PNG data:")
 i = 0
-while i < 30:
+while i < 55:
 	if i < 8:
 		print("Header Byte " + str(i) + ": " + str(raw_data[i]))
 		i += 1
 	elif i < 9:
 		header1_leng = raw_data[i:i+4]
 		header1_leng_dc = int.from_bytes(header1_leng, byteorder='big')
-		print("Length: " + str(header1_leng_dc))
+		print("Chunk 1 Length: " + str(header1_leng_dc))
 		i += 4
 	elif i < 13:
 		header1_name = raw_data[i:i+4]
@@ -45,13 +49,44 @@ while i < 30:
 		print("Interlace method: " + str(raw_data[i]))
 		i += 1
 	elif i < 30:
-		chunk2_leng = raw_data[i:i+4]
-		chunk2_leng_dc = int.from_bytes(chunk2_leng, byteorder='big')
-		print("Chunk 2 Length: " + str(chunk2_leng_dc))
+		crc1 = raw_data[i:i+4]
+		crc1_dc = int.from_bytes(crc1, byteorder='big')
+		print("CRC: " + str(crc1_dc))
+		i += 4
+	elif i < 34:
+		header2_leng = raw_data[i:i+4]
+		header2_leng_dc = int.from_bytes(header2_leng, byteorder='big')
+		print("Chunk 2 Length: " + str(header2_leng_dc))
+		i += 4
+	elif i < 38:
+		header2_name = raw_data[i:i+4]
+		header2_name = header2_name.decode('ascii')
+		print("Chunk 2 name: " + header2_name)
+		i += 4
+	elif i < 42:
+		gamma_val = raw_data[i:i+4]
+		gamma_val_dc = int.from_bytes(gamma_val, byteorder='big')
+		print("Gamma value (x10000): " + str(gamma_val_dc))
+		i += 4
+	elif i < 46:
+		crc2 = raw_data[i:i+4]
+		crc2_dc = int.from_bytes(crc2, byteorder='big')
+		print("CRC: " + str(crc2_dc))
+		i += 4
+	elif i < 50:
+		header3_leng = raw_data[i:i+4]
+		header3_leng_dc = int.from_bytes(header3_leng, byteorder='big')
+		print("Chunk 3 Length: " + str(header3_leng_dc))
+		i += 4
+	elif i < 54:
+		header3_name = raw_data[i:i+4]
+		header3_name = header3_name.decode('ascii')
+		print("Chunk 3 name: " + header3_name)
 		i += 4
 	else:
 		print("Byte " + str(i) + ": " + str(raw_data[i]))
 		i += 1
+
 
 idat_index = raw_data.find(b"IDAT")
 idat_info = True
