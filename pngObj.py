a1027fda2ea9f9a5aebd5dba1f85e58704f6a5f9
jonorga <55115647+jonorgan@users.noreply.github.com>
2022-04-10 21:43:48 -0400

    Added color scramble function

--- pngObj.py
+++ pngObj.py
@@ -1,5 +1,4 @@
 import struct, zlib, math
-import time
 
 class png_obj:
 	def __init__(self, png_file):
@@ -526,6 +525,145 @@ class png_obj:
 
 		f.close()
 
+	def ColorScramble(self):
+		new_file = self.file_name[:-4] + "_new.png"
+		f = open(new_file, 'wb')
+		byte_arr = [137, 80, 78, 71, 13, 10, 26, 10]
+		bin_format = bytearray(byte_arr)
+		f.write(bin_format)
+
+		# IHDR length
+		header_length_i = 13
+		header_length_b = header_length_i.to_bytes(4, 'big')
+		f.write(header_length_b)
+
+		# IHDR name
+		ihdr_name_s = "IHDR"
+		ihdr_section = ihdr_name_s.encode('ascii')
+
+		# PNG Width (4 bytes)
+		width_i = self.width_dc
+		ihdr_section += width_i.to_bytes(4, 'big')
+
+		# PNG Height (4 bytes)
+		height_i = self.height_dc
+		ihdr_section += height_i.to_bytes(4, 'big')
+
+		# Bit depth (1 byte) 8
+		bit_depth_i = 8
+		ihdr_section += bit_depth_i.to_bytes(1, 'big')
+
+		# Color type (1 byte) 6
+		color_type_i = 6
+		ihdr_section += color_type_i.to_bytes(1, 'big')
+
+		# Compression (1 byte) 0
+		compression_i = 0
+		ihdr_section += compression_i.to_bytes(1, 'big')
+
+		# Filter (1 byte) 0
+		filter_i = 0
+		ihdr_section += filter_i.to_bytes(1, 'big')
+
+		# Interlace (1 byte) 0
+		interlace_i = 0
+		ihdr_section += interlace_i.to_bytes(1, 'big')
+		f.write(ihdr_section)
+
+		# IHDR CRC (4 bytes)
+		ihdr_crc_i = zlib.crc32(ihdr_section)
+		ihdr_crc_b = ihdr_crc_i.to_bytes(4, 'big')
+		f.write(ihdr_crc_b)
+
+		# IDAT section (data length, IDAT name, data, CRC)
+		# IDAT data
+		i = 0
+		idat_byte_arr = []
+		column = 0
+		total_pix = len(self.red_arr)
+		scram = 0
+		while i < len(self.red_arr):
+			if column == 0:
+				idat_byte_arr.append(0)
+
+			if scram == 0:
+				idat_byte_arr.append(self.red_arr[i])
+				idat_byte_arr.append(self.green_arr[i])
+				idat_byte_arr.append(self.blue_arr[i])
+				idat_byte_arr.append(self.alpha_arr[i])
+			elif scram == 1:
+				idat_byte_arr.append(self.alpha_arr[i])
+				idat_byte_arr.append(self.red_arr[i])
+				idat_byte_arr.append(self.green_arr[i])
+				idat_byte_arr.append(self.blue_arr[i])
+			elif scram == 2:
+				idat_byte_arr.append(self.blue_arr[i])
+				idat_byte_arr.append(self.alpha_arr[i])
+				idat_byte_arr.append(self.red_arr[i])
+				idat_byte_arr.append(self.green_arr[i])
+			elif scram == 3:
+				idat_byte_arr.append(self.green_arr[i])
+				idat_byte_arr.append(self.blue_arr[i])
+				idat_byte_arr.append(self.alpha_arr[i])
+				idat_byte_arr.append(self.red_arr[i])
+
+			if scram == 3:
+				scram = 0
+			else:
+				scram += 1
+
+			perc = format((i / total_pix)*100, '.2f')
+			print("Writing image... " + str(perc) + "%", end="\r")
+
+			i += 1
+			if column == self.width_dc - 1:
+				column = 0
+				if scram == 3:
+					scram = 0
+				else:
+					scram += 1
+			else:
+				column += 1
+
+		print("Writing image... Complete")
+
+		idat_byte_b = bytearray(idat_byte_arr)
+		idat_byte_comp = zlib.compress(idat_byte_b)
+
+		# IDAT length
+		data_length_i = len(idat_byte_comp)
+		data_length_b = data_length_i.to_bytes(4, 'big')
+		f.write(data_length_b)
+
+		# IDAT name
+		data_name_s = "IDAT"
+		data_section = data_name_s.encode('ascii')
+		data_section += idat_byte_comp
+		f.write(data_section)
+
+		# IDAT CRC
+		idat_crc_i = zlib.crc32(data_section)
+		idat_crc_b = idat_crc_i.to_bytes(4, 'big')
+		f.write(idat_crc_b)
+
+		# IEND section (data length, IEND name, CRC)
+		# IEND length
+		iend_length_i = 0
+		iend_length_b = iend_length_i.to_bytes(4, 'big')
+		f.write(iend_length_b)
+
+		# IEND name
+		iend_name_s = "IEND"
+		iend_name_b = iend_name_s.encode('ascii')
+		f.write(iend_name_b)
+
+		# IEND CRC
+		iend_crc_i = zlib.crc32(iend_name_b)
+		iend_crc_b = iend_crc_i.to_bytes(4, 'big')
+		f.write(iend_crc_b)
+
+		f.close()
+
 
 img = png_obj("download og.png")
-img.FlipColors()
+img.ColorScramble()
